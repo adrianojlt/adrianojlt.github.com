bcb23615488b204614aa809dedf3104490547a56
adrianojlt <adrianojlt@gmail.com>
2013-04-16 17:13:41 +0100

    navigation changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,22 @@
 
 $(document).ready(function() {
+
+	var nav = $('#mainMenu a');
+
+	$(nav[0]).parent().addClass('active');
 	
+	nav.on('click', function(e) {
+
+		e.preventDefault();
+
+		var htmUrl = 'partials/' + $(this).attr('href');
+
+		$('#mainMenu li').removeClass('active');
+		$(this).parent().addClass('active');
+
+		$('#mainContent').hide().load(htmUrl, function(){
+      		$(this).fadeIn(400);
+    	});
+	});
+
 });
