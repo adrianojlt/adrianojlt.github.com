640d46c7256b99d8f418fd641d6fa287599229a6
adriano teixeira <adriano.teixeira@pmonteiro.pt>
2014-11-18 18:38:34 +0000

    ...

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,24 +13,21 @@ var app = angular.module('tutorialWebApp', [
 /**
  * Configure the Routes
  */
- /*
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    .when("/clipx",         {templateUrl: "partials/clipx.html", controller: "PageCtrl"})
+    .when("/serviceManager",{templateUrl: "partials/serviceManager.html", controller: "PageCtrl"})
+    .when("/contact",       {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    .when("/about",         {templateUrl: "partials/about.html", controller: "PageCtrl"})
     // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    .when("/acc3menu", {templateUrl: "partials/accordionMenu.html", controller: "BlogCtrl"})
+    //.when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
-*/
 
 /**
  * Controls the Blog
