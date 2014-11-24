60f52bca9a6158ec5b486179c85bdb337ecf9acd
adriano teixeira <adriano.teixeira@pmonteiro.pt>
2014-11-24 09:12:44 +0000

    removed code

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,12 +57,8 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   console.log("Page Controller reporting for duty.");
 
   // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
+  $('.carousel').carousel({interval: 5000 });
 
   // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  })
+  $('.tooltip-social').tooltip({selector: "a[data-toggle=tooltip]"})
 });
